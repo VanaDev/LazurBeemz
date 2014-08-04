71183af0d2b0183d7ab1428b450890f729b637a2
LazyBui <Lazy.Bui@Gmail.com>
2014-08-04 14:43:40 -0500

    Whoops, left in the value I was using

--- generate_development_environment.bat
+++ generate_development_environment.bat
@@ -4,7 +4,7 @@ REM Example Visual Studio toolset version: 120
 REM You can identify this from the project properties in a Vana project
 REM e.g. Right click ChannelServer -> Properties -> General -> General -> Platform Toolset: Visual Studio 2013 (v120)
 
-SET toolset=120
+SET toolset=FIXME
 
 IF "%toolset%"=="FIXME" (
 	ECHO Variables must be set before a directory structure can be compiled
